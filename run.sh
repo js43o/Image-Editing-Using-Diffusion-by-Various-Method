69bba7d20562e55075993a93dfa572f0b8f0b961
kevin20012 <kevin20012@naver.com>
2025-03-17 14:40:13 +0900

    update shell script run.sh and env fileþ

--- run.sh
+++ run.sh
@@ -3,20 +3,20 @@
     # [p2p, masactrl, pix2pix_zero, pnp]
 
 # p2p only!
-# python -u main.py --data_path img \
-#                 --output_path output \
-#                 --edit_method_list directinversion+p2p
+conda run -n p2p --no-capture-output python -u main.py --data_path img \
+                --output_path output \
+                --edit_method_list directinversion+p2p
 
 # masactrl only!
-# conda run -n masactrl python -u main.py --data_path img \
-#                 --output_path output \
-#                 --edit_method_list null-text-inversion+masactrl
+conda run -n masactrl --no-capture-output python -u main.py --data_path img \
+                --output_path output \
+                --edit_method_list null-text-inversion+masactrl
 # # pix2pix_zero only!
-python -u main.py --data_path img \
+conda run -n pix2pix_zero --no-capture-output python -u main.py --data_path img \
                 --output_path output \
                 --edit_method_list directinversion+pix2pix_zero
 
 # # pnp only!
-# conda run -n pnp python -u main.py --data_path img \
-#                 --output_path output \
-#                 --edit_method_list negative-prompt-inversion+pnp
+conda run -n pnp --no-capture-output python -u main.py --data_path img \
+                --output_path output \
+                --edit_method_list negative-prompt-inversion+pnp
